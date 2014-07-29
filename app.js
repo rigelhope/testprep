175f3d0aac2b190038ef72f58faaed1dae112737
rigel <me@wrinklynose>
2014-07-28 19:44:32 -0700

    added notes, tried something with new controller that doesnt work

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-var app = angular.module('testprep', ['ngSanitize']);
+(function() {
 
-app.controller('testprepController', function($scope,$http){
-    var qbank = [];
-    var qidList = [];
-    var qstem = "";
-
-    $http.get('generated.json').then(function(result) {
-        qbank = result.data;
-        for (var i=0, l=qbank.length;i<l;i++) {
-            qidList.push(qbank[i].id);
-        };
-        $scope.qidList = qidList;
-
-        /* this can actually go in a setter function, and initialize the array pointer to [0] */
-        $scope.qstem = qbank[0].prompt;
-        $scope.answerList = qbank[0].answerList;
-    })
-
+    var app = angular.module('testprep', ['ngSanitize']);
     
-});
+    app.controller('testprepController', function($scope,$http){
+        var qbank = [];
+        var qidList = [];
+        var qstem = "";
+    
+        $http.get('generated.json').then(function(result) {
+            qbank = result.data;
+            for (var i=0, l=qbank.length;i<l;i++) {
+                qidList.push(qbank[i].id);
+            };
+            $scope.qidList = qidList;
+    
+            /* this can actually go in a setter function, and initialize the array pointer to [0] */
+            $scope.qstem = qbank[0].prompt;
+            $scope.answerList = qbank[0].answerList;
+        })
+    
+        
+        
+    })
+    .controller('peekController', function($scope) {
+        var showing = true;
 
+        this.showIt = function(showing) {
+            showing = true;
+        };
+    });
+})();
