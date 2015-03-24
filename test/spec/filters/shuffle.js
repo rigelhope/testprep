dc1a969eb34dc1822a23153192ac36d46785f25d
rigel <me@rigelhope.org>
2015-03-23 21:16:38 -0700

    added metadata files and (thus far unused) testing directory

---
+++ test/spec/filters/shuffle.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Filter: shuffle', function () {
+
+  // load the filter's module
+  beforeEach(module('testprepApp'));
+
+  // initialize a new instance of the filter before each test
+  var shuffle;
+  beforeEach(inject(function ($filter) {
+    shuffle = $filter('shuffle');
+  }));
+
+  it('should return the input prefixed with "shuffle filter:"', function () {
+    var text = 'angularjs';
+    expect(shuffle(text)).toBe('shuffle filter: ' + text);
+  });
+
+});
