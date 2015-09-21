9239abfc1a8709f90d6b32fff02d520d16354695
rigel <me@wrinklynose>
2015-09-21 13:30:21 -0700

    more cleanup of dependencies

--- app/scripts/services/dataservice.js
+++ app/scripts/services/dataservice.js
@@ -9,9 +9,7 @@
  * Service in the testprepApp.
  */
 angular.module('testprepApp')
-  .service('DataService', function($http, $filter) {
-//angular.module('testprepApp', ['LocalForageModule'])
-//  .service('DataService', function($http, $filter, $localForage) {
+  .service('DataService', function($http, $filter, $localForage) {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
     function Question(rawData) {
