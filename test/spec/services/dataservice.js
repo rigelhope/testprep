0e00f951b6b49369e79f2e508c656cbe7ac4f005
rigel <me@wrinklynose>
2015-09-21 20:14:23 -0700

    skeleton tests for session service (which isnt written yet)

--- test/spec/services/dataservice.js
+++ test/spec/services/dataservice.js
@@ -5,20 +5,31 @@
 describe('Service: DataService', function () {
 
   // load the service's module
-  beforeEach(module('testprepApp'));
+  //beforeEach(module('testprepApp'));
 
 
   // instantiate service
   var DataService;
-  beforeEach(inject(function (_DataService_) {
-    DataService = _DataService_;
-  }));
+  beforeEach(function() {
+      //load service's module
+      module('testprepApp');
+
+      //inject DataService, connect an outside variable to it
+      inject(function (_DataService_) {
+        DataService = _DataService_;
+      });
+
+      //let's see if i can move the fetchData out here to DRY
+//      DataService.fetchData('generated.json');
+
+  });
 
 //  it gets json data and loads it into local memory (qbank)
   it('gets json data and loads it into a local object', function() {
     DataService.fetchData('generated.json').then(function() {
       expect(DataService.qbank.length).toBeGreaterThan(0);
     });
+//    expect(DataService.qbank.length).toBeGreaterThan(0);
   });
 
 //
@@ -32,7 +43,7 @@ describe('Service: DataService', function () {
 
 //        
 //  it makes a list of unique "subject" elements
-  it('makes a list of unique "subject" elements', function() {
+  xit('makes a list of unique "subject" elements', function() {
     DataService.fetchData('generated.json').then(function() {
       expect(DataService.subjects.length).toBeGreaterThan(0);
     });
@@ -44,7 +55,7 @@ describe('Service: DataService', function () {
 
 //
 //  it puts a subset of elements into a presentation queue
-  it('puts some of the data into a presentation queue', function() {
+  xit('puts some of the data into a presentation queue', function() {
     DataService.fetchData('generated.json').then(function(service) {
       service.loadQuestions(service.qbank)}).then(function() {
         expect(DataService.qbank).toEqual(DataService.questions);
@@ -72,15 +83,25 @@ describe('Service: DataService', function () {
 });
 
 describe('persistent storage', function() {
+
   var localForage;
-  beforeEach(module('LocalForageModule'));
+  beforeEach(function() {
+      module('LocalForageModule');
+      inject(function($localForage) {
+          localForage = $localForage;
+      });
+  });
+
+  it('tests clear function', function() {
+    localForage.clear().then(function() {
+      expect(localForage.length).toBe(0);
+    });
+  });
+
   it('tests add operations', function() {
-    inject(function($localForage) {
-      localForage = $localForage;
-      localForage.setItem('foo', 'bar').then(function(){
-        localForage.getItem('foo').then(function(data) {
-          expect(data).toBe('bar');
-        });
+    localForage.setItem('foo', 'bar').then(function(){
+      localForage.getItem('foo').then(function(data) {
+        expect(data).toBe('bar');
       });
     });
   });
@@ -97,7 +118,7 @@ describe('elaborate test', function() {
     });
   });
   
-  it('pushes stuff to localforage', function() {
+  xit('pushes stuff to localforage', function() {
     inject(function($localForage) {
       //doStuff();
       expect($localForage.foo).toHaveBeenCalled();
